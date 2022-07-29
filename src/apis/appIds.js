aa5d0bd19c357f5c823a89ed4a88ab15202adfcb
unknown <sntakirutimana72@gmail.com>
2022-07-29 18:24:13 +0200

    exclue my api credentials from codebase

---
+++ src/apis/appIds.js
@@ -0,0 +1,3 @@
+export const WEATHER_API_ID = '';
+
+export const NINJA_API_ID = '';
--- src/apis/weatherApi.js
+++ src/apis/weatherApi.js
@@ -1,8 +1,9 @@
-const API_ID = '7e1c2afc7d1aaf38616fd7dfe83427ef';
+import { WEATHER_API_ID, NINJA_API_ID } from './appIds';
+
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 
 export const getWeatherStats = (lon, lat) => fetch(
-  `${API_URL}lat=${lat}&lon=${lon}&appid=${API_ID}`,
+  `${API_URL}lat=${lat}&lon=${lon}&appid=${WEATHER_API_ID}`,
 ).then(
   (response) => response.json(),
 ).then(
@@ -13,7 +14,7 @@ export const getCities = () => fetch(
   'https://api.api-ninjas.com/v1/city?country=US&limit=100',
   {
     method: 'GET',
-    headers: { 'X-Api-Key': 'FJ/oCRaNAy12WLCv26g1hg==mrmFYXJNUb4MNlNM' },
+    headers: { 'X-Api-Key': NINJA_API_ID },
     contentType: 'application/json',
   },
 ).then(
